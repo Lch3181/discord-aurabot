e7e332e5939e30f033e8af9d57e26a9745db5697
Lch#3181 <34583022+Lch3181@users.noreply.github.com>
2023-06-13 19:27:38 -0500

    Update watchAppendedLines.ts
    buffer 10 lines before sending

--- helpers/watchAppendedLines.ts
+++ helpers/watchAppendedLines.ts
@@ -1,34 +1,42 @@
 import fs from 'fs';
 
 export function watchFile(callback: (newLine: string) => void): void {
-  const file = `${process.env.AURABOT_ADDRESS}/aura.log`;
+    const file = `${process.env.AURABOT_ADDRESS}/aura.log`;
+    const watcher = fs.watch(file, { encoding: 'utf-8' });
+    let lastPosition = fs.statSync(file).size; // Get the current size of the file
+    let previousLine: String = ""
+    let linesBuffer: String[] = []
 
-  let lastPosition = fs.statSync(file).size; // Get the current size of the file
+    watcher.on('change', (eventType, filename) => {
+        if (eventType === 'rename') {
+            // File has been renamed, do not take any action
+            return;
+        }
 
-  const watcher = fs.watch(file, { encoding: 'utf-8' });
+        const readStream = fs.createReadStream(file, {
+            encoding: 'utf-8',
+            start: lastPosition,
+        });
 
-  watcher.on('change', (eventType, filename) => {
-    if (eventType === 'rename') {
-      // File has been renamed, do not take any action
-      return;
-    }
+        readStream.on('data', (data) => {
+            const lines = (<string>data).split('\n').filter((line) => line.trim() !== ''); // Explicitly cast data to string
+            if (lines.length > 0) {
+                lastPosition += Buffer.byteLength(data); // Update the lastPosition based on the byte length of data
+                const newestLine = lines[lines.length - 1];
 
-    const readStream = fs.createReadStream(file, {
-      encoding: 'utf-8',
-      start: lastPosition,
+                if (previousLine != newestLine) {
+                    previousLine = newestLine
+                    linesBuffer.push(newestLine)
+                    if (linesBuffer.length >= 10) {
+                        callback(`\`\`\`cmd\n${linesBuffer.join('\n')}\`\`\``); // Invoke the callback function with the newest lines
+                        linesBuffer = []
+                    }
+                }
+            }
+        });
+
+        readStream.on('end', () => {
+            readStream.close();
+        });
     });
-
-    readStream.on('data', (data) => {
-      const lines = (<string>data).split('\n').filter((line) => line.trim() !== ''); // Explicitly cast data to string
-      if (lines.length > 0) {
-        lastPosition += Buffer.byteLength(data); // Update the lastPosition based on the byte length of data
-        const newestLine = lines[lines.length - 1];
-        callback(newestLine); // Invoke the callback function with the newest line
-      }
-    });
-
-    readStream.on('end', () => {
-      readStream.close();
-    });
-  });
 }
