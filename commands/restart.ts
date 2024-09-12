4a86bb0afd43c851a19f2efd57b4a5d9581d782f
Lch#3181 <34583022+Lch3181@users.noreply.github.com>
2024-09-12 02:26:42 -0400

    add restart bot command

---
+++ commands/restart.ts
@@ -0,0 +1,31 @@
+import WOKCommands, { ICommand } from "wokcommands";
+import { delay, execShellCommand } from "../helpers/global";
+
+export default {
+    name: 'restart',
+    category: 'Configuration',
+    description: 'Restart Aura Bot (root admin only)',
+    ownerOnly: (process.env.USAGE == "public") ? true:false,
+    slash: true,
+    testOnly: (process.env.NODE_ENV == "development") ? true:false,
+
+    callback: async ({ interaction, args }) => {
+        if (interaction) {
+            await interaction.reply({
+                content: "restarting arua bot"
+            })
+            
+            const result = await execShellCommand("pkill aura++")
+
+            await delay(5)
+
+            const result = await execShellCommand(`nohup ${process.env.AURABOT_ADDRESS}/aura++ > ${process.env.AURABOT_ADDRESS}/aura.log 2>&1 &`)
+
+            await delay(5)
+
+            await interaction.editReply({
+                content: "restarted"
+            })
+        }
+    },
+} as ICommand
